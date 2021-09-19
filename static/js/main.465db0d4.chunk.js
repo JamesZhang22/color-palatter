84be9cfe4c37277fe7652a9f957d0adff96e2ea2
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-09-18 23:22:39 -0400

    Updates

---
+++ static/js/main.465db0d4.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpcolor-palatter"]=this["webpackJsonpcolor-palatter"]||[]).push([[0],{10:function(t,e,r){"use strict";r.r(e);var c=r(0),n=r.n(c),o=r(3),s=r.n(o),a=(r(8),r(1));var i=function(){return Object(a.jsx)("div",{children:"colors!"})};s.a.render(Object(a.jsx)(n.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,r){}},[[10,1,2]]]);
+//# sourceMappingURL=main.465db0d4.chunk.js.map
